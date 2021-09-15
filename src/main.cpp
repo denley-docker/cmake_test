cafd70ec4bcbdd4c0b13875744d4c254c69825c6
Denley Hsiao <denley@justtodo.com>
2021-09-15 15:30:47 +0800

    Add config file.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
+#include "CMakeTestConfig.h"
 #include <iostream>
 
 int main(int argc,char* argv[])
 {
   int result = 0;
 
+  std::cout<< argv[0] << " Version "
+    << cmake_test_VERSION_MAJOR << "."
+    << cmake_test_VERSION_MINOR << "."
+    << cmake_test_VERSION_PATCH << std::endl;
   std::cout << "Hello, CMake 3.21.2" << std::endl;
   return result;
 }
