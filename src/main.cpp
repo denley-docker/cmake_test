98ee4a91be086c3815100c8ada82ce3f9ecbe839
Denley Hsiao <denley@justtodo.com>
2021-09-15 17:25:15 +0800

    Use library.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include "CMakeTestConfig.h"
 #include <iostream>
 
+#ifdef USE_MYMATH
+  #include "my_math/my_math.h"
+#else
+  #include <cmath>
+#endif
+
 int main(int argc,char* argv[])
 {
   int result = 0;
@@ -9,6 +15,13 @@ int main(int argc,char* argv[])
     << cmake_test_VERSION_MAJOR << "."
     << cmake_test_VERSION_MINOR << "."
     << cmake_test_VERSION_PATCH << std::endl;
+
+#ifdef USE_MYMATH
+  std::cout << mysqrt(5) << std::endl;
+#else
+  std::cout << std::sqrt(5) <<std::endl;
+#endif
+
   std::cout << "Hello, CMake 3.21.2" << std::endl;
   return result;
 }
